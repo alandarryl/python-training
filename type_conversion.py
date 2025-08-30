3670f856818014ea174cfcc322f6ee507f0feed4
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-08-30 13:46:13 +0200

    type conversion and string course

---
+++ type_conversion.py
@@ -0,0 +1,14 @@
+
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# # whaterver is enter in the terminal will be treated as a string
+# age = 2025 - int(birth_year)
+# print(type(age))
+# print(age)
+
+weight_lbs = input(" Enter your weight in pounds: ")
+
+weight_kg = int(weight_lbs) * 0.45
+
+print("Your weight in kilogram is " + str(weight_kg) + "kg")
