1b26730103670fb41b8b29af6262adec86b44caa
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-08-30 14:10:00 +0200

    completed strings course , start formatted strings part

---
+++ strings.py
@@ -0,0 +1,13 @@
+
+# course = ''' hi jonathan, 
+# this is my first python course. 
+# i hope you like it. '''
+
+# print(course)
+
+# course = 'Python for Beginners'
+
+# print(course[:])
+
+name = 'Jennifer'
+print(name[1:-1])
