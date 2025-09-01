abc8a21fe3e67c243f8b3cd690eea1a23e161498
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-09-01 23:45:31 +0200

    starting loop

--- statements.py
+++ statements.py
@@ -1,19 +1,43 @@
 
-user_input = input(' what is the weather today')
+# if statement
 
-weather_today = user_input.lower()
+# user_input = input(' what is the weather today ? ')
 
-if weather_today == 'rainy':
-    print("bring an umbrella")
-elif weather_today == 'sunny':
-    print("wear a hat")
-elif weather_today == 'snowy':
-    print("wear a coat")
-elif weather_today == 'cloudy':
-    print("wear a jacket")
-else:
-    print("i don't know what to wear today")
+# weather_today = user_input.lower()
+
+# if weather_today == 'rainy':
+#     print("bring an umbrella")
+# elif weather_today == 'sunny':
+#     print("wear a hat")
+# elif weather_today == 'snowy':
+#     print("wear a coat")
+# elif weather_today == 'cloudy':
+#     print("wear a jacket")
+# else:
+#     print("i don't know what to wear today")
     
 
+# user_entry = input(' have you a good credy yes or no ? ')
+
+# user_entry = user_entry.lower()
+
+# if user_entry == 'yes':
+#     has_good_credit = True
+# else:
+#     has_good_credit = False
+
+# price = 1000000
+
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+
+# print(f'down payment: ${down_payment}')
+
+
+# logical operators
+
+# operator and, or, not, is, is not, in, not in
 
 
