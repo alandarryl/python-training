20cbfa50a0e2f6248863547b551ac2611330fbd6
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-09-05 09:30:35 +0200

    list file update

---
+++ lists.py
@@ -0,0 +1,65 @@
+
+# numbers = [2, 5, 7, 8, 144, 58, 15, 24]
+
+# greater_number = 0
+
+# for number in numbers:
+#     if greater_number < number:
+#         greater_number = number
+# print(f'the biggest number of our list is {greater_number}')
+
+# matrix = [
+#     [1, 1, 0],
+#     [1, 0, 1],
+#     [0, 1, 0]
+# ]
+# matrix[0][1] = 20
+# coordinate = matrix[0][1]
+
+# print(coordinate)
+
+# for row in matrix:
+#     for colon in row:
+#         print(colon)
+
+
+# list method
+
+# numbers = [5, 2, 1, 7, 4, 7, 8, 8, 7]
+
+# numbers.append(8)
+# numbers.insert(0, 10)
+# numbers.remove(7)
+# numbers.clear()
+# numbers.pop()
+# numbers.index(7)
+# numbers.remove(7)
+
+# numbers.sort()
+# numbers.reverse()
+
+# numbers2 = numbers.copy()
+# numbers.append(10)
+
+# for n in numbers:
+#     while numbers.count(n) > 1:
+#         numbers.remove(n)
+# print(numbers)
+
+# for n in numbers:
+#     if numbers.count(n) > 1:
+#         numbers.remove(n)
+# print(numbers)
+# uniques = []
+# for n in numbers:
+#     if numbers.count(n) < 2:
+#         unique.append(n)
+# print(unique)
+
+# for number in numbers:
+#     if number not in uniques:
+#         uniques.append(number)
+# print(uniques)
+
+
+
